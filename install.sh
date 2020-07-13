415e6f5c393f34b50ac2049185e0417be5111494
codechenx <codechenx@gmail.com>
2020-07-14 00:05:55 +0800

    bug fixes

--- install.sh
+++ install.sh
@@ -32,10 +32,12 @@ echo "Detected platform: $platform"_"$architecture"
 TAG=`githubLatestTag codechenx/tv`
 
 echo "Downloading https://github.com/codechenx/tv/releases/download/v$TAG/tv_"$TAG"_"$platform"_"$architecture".tar.gz"
-curl -L "https://github.com/codechenx/tv/releases/download/v$TAG/tv_"$TAG"_"$platform"_"$architecture".tar.gz" > tv.tar.gz
-tar -zxf tv.tar.gz
+curl -L "https://github.com/codechenx/tv/releases/download/v$TAG/tv_"$TAG"_"$platform"_"$architecture".tar.gz" > tv_"$TAG"_"$platform"_"$architecture".tar.gz
+tar -zxf tv_"$TAG"_"$platform"_"$architecture".tar.gz
+rm -f tv_"$TAG"_"$platform"_"$architecture".tar.gz
 chmod +x tv
 
-echo "\033[33mThis script will download tv binary file to your current directory
+echo "#################################################################################
+This script will download tv binary file to your current directory
 you need to run sudo cp tv /usr/local/bin/ 
 or copy tv binary file to any directory which is in the environment variable PATH"
